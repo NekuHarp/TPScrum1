670562a82b1697a94a8e887432f5150cc4573dd1
Pho3 <rainbowdashtheone@gmail.com>
2018-12-03 15:19:41 +0100

    Work recursively

--- Abstract.py
+++ Abstract.py
@@ -1,20 +1,33 @@
 import re
 import sys
+import glob
+import subprocess
 
 inW = False
 
 regex = "Abstract.*\n"
 eol = "\n"
 
-abst = ""
+gl = glob.glob('./*.pdf')
 
-file = open(sys.argv[1], "r")
 
-for line in file:
-    if re.search(regex, line):
-        inW = True
-    if line == eol:
-        inW = False
-    if inW:
-        abst += line[:-1]
-print(abst)
+for g in gl:
+    print("\n")
+    fname = '{0}.txt'.format(g[:-4])
+    print(g)
+    print(fname)
+    p = subprocess.Popen(['pdftotext' , g , fname])
+    p.wait()
+
+    file = open(fname, "r")
+    abst = ""
+
+    for line in file:
+        if re.search(regex, line):
+            inW = True
+        if line == eol:
+            inW = False
+        if inW:
+            abst += ' '
+            abst += line[:-1]
+    print(abst)
